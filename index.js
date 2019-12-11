3afdd39b9cbf401f1cb2d48dbd8565addc8e9f14
Jesse Edwards <jesse.david.edwards.1987@gmail.com>
2019-12-10 19:57:51 -0600

    index.js modified

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ function startProg() {
     const axios = require("axios");
     const
     const fs = require("fs");
-    let name;
-    let company;
-    let actualName;
-    let urlImage;
-    let location;
-    let githubProfile;
-    let followers;
-    let stars;
-    let repos;
-    let following;
-    let favColor;
-    
+    let nameQ;
+    let companyQ;
+    let actualNameQ;
+    let urlImageQ;
+    let locationQ;
+    let githubProfileQ;
+    let followersQ;
+    let starsQ;
+    let reposQ;
+    let followingQ;
+    let favColorQ;
+
     inquirer.prompt([{
         message: "Enter github username",
         name: "enteredName"
@@ -23,16 +23,32 @@ function startProg() {
         message: "What is your favorite color?",
         name: "enteredColor"
     }])
-    .then(function ({ enteredName, enteredColor }) {
-        username = enteredName;
-        favColor = enteredColor;
-        const queryUrl = `https://api.github.com/users/${username}`;
-        gitQuery(queryUrl);
-    });
-    Function gitQuery()
+        .then(function ({ enteredName, enteredColor }) {
+            userName = enteredName;
+            favColor = enteredColor;
+            const queryUrl = `https://api.github.com/users/${username}`;
+            gitQuery(queryUrl);
+        });
 
-    
-    
+    function gitQuery(URL) {
+        axios.get(url)
+            .then(function (response) {
+                companyQ = response.data.company;
+                actualNameQ = response.data.name;
+                urlImageQ = response.data.avatar_url;
+                locationQ = response.data.location;
+                githubProfileQ = response.data.html_url;
+                followersQ = response.data.followers;
+                starsQ = response.data.starsQ;
+                reposQ = response.data.public_repos;
+                followingQ = response.data.following;
+                const gitQuery = `https://api.github.com/users/${username}`;
+                buildFIle(gitQuery);
+            
+
+
+
+
 
 
 
